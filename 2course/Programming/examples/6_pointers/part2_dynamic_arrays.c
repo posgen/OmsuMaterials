d41e2c56a378a4c51da67712ec1e000916eee078
Evgeny <pospelov.evgeny@gmail.com>
2016-04-01 01:21:51 +0600

    final dynamic arrays simple example with explanation

--- 2course/Programming/examples/6_pointers/part2_dynamic_arrays.c
+++ 2course/Programming/examples/6_pointers/part2_dynamic_arrays.c
@@ -86,10 +86,11 @@
     объявлены в заголовочном файле <stdlib.h>. Основные из них:
         malloc и calloc
     Они объявляются как:
-        void* malloc(size_t size) - функция возращает блок памяти размером size (size_t - специальный тип для хранения размеров размеров, обычно безнаковое целое)
+        void* malloc(size_t size) - функция возращает блок памяти размером size
+            (size_t - специальный тип для хранения размеров размеров, обычно безнаковое целое - unsigned int или аналог)
 
-        void* calloc(size_t num, size_t size) - функция возращает блок памяти размером num * size, при этом весь возращаемый блок будет обнулён (все биты
-                                                устанавливаются в нуль)
+        void* calloc(size_t num, size_t size) - функция возращает блок памяти размером num * size, при
+            этом весь возращаемый блок будет обнулён (все биты устанавливаются в нуль)
 
     Освобождается память (удаляется ранее выделенный блок памяти) с помощью функции free:
         void free(void* ptr) - в неё передаётся указатель, который был выделен с помощью функций malloc/realloc.
@@ -98,6 +99,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> // используется для генератора псевдослучайных чисел
 
 void examples_with_static_arrays(void)
 {
@@ -107,15 +109,124 @@ void examples_with_static_arrays(void)
     // записываем в p_int адрес пятого элемента
     p_int = &arr[4];
 
-    printf("Current value of p_int is %d\n", *p_int); // печатаем пятый элемент через указатель и разыменование
-    printf("Current value of p_int is %d\n", p_int[2]); // печатаем седьмой элемент через указатель и индексацию
-                                                        // p_int[2] - сдвиг вправо на 2 позиции относительно начального положения p_int
+    // печатаем пятый элемент через указатель и разыменование
+    printf("Current value of p_int is %d\n", *p_int);
 
-    printf("Current value of p_int is %d\n", p_int[-1]); // печатаем четвёртый элемент через указатель и индексацию. Да, индекс может быть отрицательным
+    // печатаем седьмой элемент через указатель и индексацию
+    // p_int[2] - сдвиг вправо на 2 позиции относительно начального положения p_int
+    printf("Current value of p_int is %d\n", p_int[2]);
+
+    // печатаем четвёртый элемент через указатель и индексацию. Да, индекс может быть отрицательным
+    printf("Current value of p_int is %d\n", p_int[-1]);
+}
+
+// Передаём в функцию 2 аргумента: размер массива типа float и размер массива типа int
+void examples_of_dynamic_arrays(unsigned int float_arr_size, unsigned int int_arr_size)
+{
+    // Для работы с динамическим массивом необходим указатель
+    float *f_arr;
+    int *i_arr1, *i_arr2;
+    unsigned int index;
+
+    /*
+        Выделяем блок памяти под массив из float_arr_size элементов.
+        Нужное количество памяти вычисляется по формуле:
+            кол_во_элементов * размер_типа_элемента
+
+        Функция malloc просто возращает блок памяти нужного размера. Вообще говоря,
+        этот блок может быть заполнен какими-то данными (зависит от ОС).
+
+        В случае, если память выделить невозможно, функция возращает нулевой указатель - NULL.
+        Под "нулевым" понимается адрес, который гарантированно не указывает ни на одну ячейку памяти.
+        Вообще говоря, после каждого выделения необходима проверка того, была ли реально выделена память
+    */
+    f_arr = (float*) malloc(float_arr_size * sizeof(float));
+    if ( f_arr == NULL ) {
+        perror("Error with memory initialization"); // печатаем сообщение в стандартный поток для ошибок
+        return; // выходим принудительно из функции
+    }
+
+    // Аналогичное выделение динамического массива под массив типа int
+    i_arr1 = (int*) malloc(int_arr_size * sizeof(int));
+    if ( i_arr1 == NULL ) {
+        free( f_arr ); // если до сюда дошло, то массив под f_arr был выделен успешно, поэтому надо его удалить из памяти
+        perror("Error with memory initialization"); // печатаем сообщение в стандартный поток для ошибок
+        return; // выходим принудительно из функции
+    }
+
+    /*
+        calloc отличается от malloc 2-мя вещами:
+            1) синтаксис: calloc принимает два аргумента - количество элементов и размер каждого из них
+            2) calloc инициализирует всю выделенную память нулями:
+                то есть,
+                    i_arr2[0] == 0
+                    i_arr2[1] == 0
+                    i_arr2[2] == 0
+                    и так далее ...
+
+                В случае malloc - не факт, что i_arr1[0] или i_arr1[1] - будут равны нулю. Возможно, там
+                останутся другие значения (ещё раз, записит от выделения памяти в ОС).
+    */
+    i_arr2 = (int*) calloc(int_arr_size, sizeof(int));
+    if ( i_arr2 == NULL ) {
+        free( f_arr );
+        free( i_arr1 );
+        perror("Error with memory initialization"); // печатаем сообщение в стандартный поток для ошибок
+        return; // выходим принудительно из функции
+    }
+
+
+    // заполняем и печатаем выше выделенные массивы значениями
+    printf("\nFloat array:\n");
+    for (index = 0; index < float_arr_size; ++index) {
+        f_arr[index] = (float) rand() / RAND_MAX; // используется явное приведение к типу float - (float) - для того,
+                                                  // чтобы операция деления была не целочисленной
+        printf("%lf ", f_arr[index]);
+    }
+
+    printf("\nInt arrays:\n");
+    for (index = 0; index < int_arr_size; ++index) {
+        i_arr1[index] = rand() % 22;
+        i_arr2[index] = rand() % 8;
+
+        printf("i_arr1[%d] = %d  ", index, i_arr1[index]);
+        printf("i_arr2[%d] = %d\n", index, i_arr2[index]);
+    }
+
+    /*
+        Если нужно изменить (увеличить/уменьшить) блок уже выделенной динамической памяти,
+        то к вашим услугам функция realloc:
+            void* realloc(void *pointer, size_t size) - где pointer - указатель на существующий динамический массив,
+                                                       size - новый размер памяти.
+    */
+    unsigned int new_size = int_arr_size + 10;
+    i_arr2 = (int*) realloc(i_arr2, new_size * sizeof(int)); // увеличивыем массив i_arr2 на 10 элементов относительно числа int_arr_size
+    if (i_arr2 == NULL ) {
+        free(f_arr);
+        free(i_arr1);
+
+        perror("Error with memory reallocation");
+        return;
+    }
+
+    printf("\ni_arr2, last 10 elements:\n");
+    for (index = int_arr_size; index < new_size; ++index) {
+        // заполняем новые 10 элементов в i_arr2
+        i_arr2[index] = -5 + rand() % 6;
+        printf("i_arr2[%d] = %d\n", index, i_arr2[index]);
+    }
+
+
+    free(f_arr);
+    free(i_arr1);
+    free(i_arr2);
 }
 
 int main(void)
 {
+    srand( time(NULL) );
     examples_with_static_arrays();
+
+    examples_of_dynamic_arrays(10, 20);
     return 0;
 }
