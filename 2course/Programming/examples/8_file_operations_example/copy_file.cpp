3ea131b9b49ad288dbe2c3f4c61ddda2b2ced038
Evgeny <pospelov.evgeny@gmail.com>
2017-12-04 21:03:25 +0600

    Update file operations examples with C-versions of copy action

--- 2course/Programming/examples/8_file_operations_example/copy_file.cpp
+++ 2course/Programming/examples/8_file_operations_example/copy_file.cpp
@@ -1,5 +1,5 @@
 /*
-    Пример простого копирования файла.
+    Пример копирования файла через промежуточный буфер
 
 */
 #include <iostream>
@@ -10,22 +10,22 @@ using namespace std;
 int main()
 {
     /*
-    Класс ifstream используется для создания потока на чтение файла.
-    Конструктор принимает два параметра:
-        char * - имя файла
-        mode   - флаги для открытия файла. Сами флаги являются отдельным типом, который сводится к целому значению.
-                 Флаги комбинируются с помощью оператора | (исключающее или). Значение по умолчанию - std::ios_base::in
+        Класс ifstream используется для создания потока на чтение файла.
+        Конструктор принимает два параметра:
+            char * - имя файла
+            mode   - флаги для открытия файла. Сами флаги являются отдельным типом, который сводится к целому значению.
+                     Флаги комбинируются с помощью оператора | (исключающее или). Значение по умолчанию - std::ios_base::in
 
-    Виды mode:
-        std::ios_base::app      - добавление в конец файла
-        std::ios_base::ate      - открыть и найти конец файла
-        std::ios_base::binary   - режим чтение/запись в двоичном режиме (зависит от ОС)
-        std::ios_base::in       - открытие потока на чтение
-        std::ios_base::out      - открытие потока на запись
-        std::ios_base::trunc    - при открытии потока установить длину файлав 0.
+        Виды mode:
+            std::ios_base::app      - добавление в конец файла
+            std::ios_base::ate      - открыть и найти конец файла
+            std::ios_base::binary   - режим чтение/запись в двоичном режиме (зависит от ОС)
+            std::ios_base::in       - открытие потока на чтение
+            std::ios_base::out      - открытие потока на запись
+            std::ios_base::trunc    - при открытии потока установить длину файлав 0.
 
-    Пример:
-        ifstream in_file("some_file.dat", std::ios_base::in | std::ios_base::binary);
+        Пример:
+            ifstream in_file("some_file.dat", std::ios_base::in | std::ios_base::binary);
     */
     ifstream in_file("input.txt");
 
@@ -54,27 +54,31 @@ int main()
             Для выходного потока (ofstream, fstream) существуют аналогичные функции для перемещению по файлу - seekp / tellp.
             Они устанавливают позицию для записи следующего символа.
         */
-        in_file.seekg (0, in_file.end);
-        long size = in_file.tellg();
-        in_file.seekg (0);
+        if (out_file.open()) {
+            in_file.seekg (0, in_file.end);
+            long size = in_file.tellg();
+            in_file.seekg (0);
 
-        // Буфер для считывания из файла
-        char* buffer = new char[size];
+            // Буфер для считывания из файла
+            char* buffer = new char[size];
 
-        // Функция read считывает size байт в область, заданную указателем buffer
-        in_file.read(buffer, size);
+            // Функция read считывает size байт в область, заданную указателем buffer
+            in_file.read(buffer, size);
 
-        // Функция write аналогична по аргументам read, разве что пишет из указанной области size байт в выходной поток
-        out_file.write(buffer, size);
+            // Функция write аналогична по аргументам read, разве что пишет из указанной области size байт в выходной поток
+            out_file.write(buffer, size);
 
-        delete[] buffer;
+            delete[] buffer;
 
-        out_file.close();
-        in_file.close();
+            out_file.close();
+            in_file.close();
 
-        cout << "Coping complete" << endl;
+            cout << "Coping complete" << endl;
+        } else {
+            cerr << "Error with opening output file" << endl;
+        }
     } else {
-        cout << "Error with opening file" << endl;
+        cerr << "Error with opening file" << endl;
     }
 
     return 0;
