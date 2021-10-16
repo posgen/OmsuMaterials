d603dd3c306c9138f10f37c7a0ae337ff801aeac
Evgeny <pospelov.evgeny@gmail.com>
2021-10-16 19:47:47 +0600

    same formatting for include and source files

--- 2course/Programming/2021_2022/lectures/examples/lecture3/include/liblect3.h
+++ 2course/Programming/2021_2022/lectures/examples/lecture3/include/liblect3.h
@@ -6,26 +6,26 @@
 class FP003Array
 {
 public:
-    FP003Array() = default;
-    FP003Array(size_t arr_sz);
-    FP003Array(const FP003Array& other);
-    ~FP003Array();
+  FP003Array() = default;
+  FP003Array(size_t arr_sz);
+  FP003Array(const FP003Array& other);
+  ~FP003Array();
 	
-    FP003Array& operator=(const FP003Array& rhs);
-    int& operator[](size_t index);
-    // Специальная перегрузка для возможности использования констант
-    // данного типа (как напрямую, так и в качестве параметров функций)
-    const int& operator[](size_t index) const;
+  FP003Array& operator=(const FP003Array& rhs);
+  int& operator[](size_t index);
+  // Специальная перегрузка для возможности использования констант
+  // данного типа (как напрямую, так и в качестве параметров функций)
+  const int& operator[](size_t index) const;
     
-    size_t length() const;
-    void push(int new_elem);
+  size_t length() const;
+  void push(int new_elem);
 
 private:
-    int *data_ = nullptr;
-    size_t length_ = 0;
-    size_t capacity_ = 0;
+  int *data_ = nullptr;
+  size_t length_ = 0;
+  size_t capacity_ = 0;
     
-    void re_allocate();
+  void re_allocate();
 };
 
 #endif
